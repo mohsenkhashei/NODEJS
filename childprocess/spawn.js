09cb4f384d51417a2da0c572d395409b713b459a
Mohsen <m.khasheii@gmail.com>
2022-11-04 23:02:16 +0330

    update spawn

--- childprocess/spawn.js
+++ childprocess/spawn.js
@@ -1,6 +1,8 @@
 const { spawn } = require('child_process');
 
-const child = spawn('ls', ['-lh']);
+// const child = spawn('ls', ['-lh'], {stdio: 'inherit'}); // using parent
+const child = spawn('ls', ['-lh'], { stdio: 'pipe' }); // default listen to data
+// const child = spawn('ls', ['-lh'], { stdio: 'ignore' }); //  not intrested to listen to anything from child
 
 child.stdout.on('data', (data) => {
 	console.log(`stdout: ${data}`);
@@ -15,5 +17,5 @@ child.on('error', (error) => console.log(`error: ${error.message} `));
 child.on('exit', (code, signal) => {
 	if (code) console.log(`Process exit with code: ${code}`);
 	if (signal) console.log(`Process killed with signal: ${signal}`);
-    console.log(`Done 😀`)
+	console.log(`Done 😀`);
 });
